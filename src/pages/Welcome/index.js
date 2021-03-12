c04e982611c03151a084f2c21ebb50937a9874e1
Julieta Mullen <67779109+julietamullen@users.noreply.github.com>
2021-03-12 20:33:44 -0300

    Add files via upload

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -12,7 +12,7 @@ function Welcome () {
             <p>This is a simple app aimed to help you take your first steps in japanese!</p>
             <p>Hiragana is one of the two syllabaries (along with katakana) that are used in the japanese writting system, and it's a phonetic lettering system, which means that each hiragana is associated to a sound.</p>
             <p>In this app, you will find the flashcards sorted by groups (as most charts are), or you can generate a random selection. In the frontside of each flashcard you'll see the hiragana, and in the backside, the sound it is associated to. Just click/tap them to turn them around!</p>
-            <p><b>がんばって!</b></p>
+            <p className="goodluck"><b>がんばって!</b></p>
             <section><Link to="../Flashcards"><button>Start</button></Link></section>
             </div>
     </div>
